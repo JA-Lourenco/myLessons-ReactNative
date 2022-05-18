2b569c6e1a53e91cc0ae23ffd952e159ef1c8c10
JA-Lourenco <ja.lourenco@outlook.com>
2022-05-18 10:18:57 -0300

    Created app with expo

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Lessons } from './src/screens/Lessons';
+
+export default function App() {
+  return (
+    <Lessons />
+  );
+}
---
+++ src/screens/Lessons/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import {
+    Container,
+    Title
+} from './styles';
+
+export function Lessons(){
+    return (
+        <Container>
+            <Title>Hello World!</Title>
+        </Container>
+    )
+}
